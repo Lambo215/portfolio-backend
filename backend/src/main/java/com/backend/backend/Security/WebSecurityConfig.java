818ed1d8420bdae4a0917b16e3024865ef0c2226
Lambo215 <97458396+Lambo215@users.noreply.github.com>
2023-03-26 22:48:59 -0300

    Update WebSecurityConfig.java

--- backend/src/main/java/com/backend/backend/Security/WebSecurityConfig.java
+++ backend/src/main/java/com/backend/backend/Security/WebSecurityConfig.java
@@ -1,7 +1,7 @@
 package com.backend.backend.Security;
 
 import java.util.Arrays;
-
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,7 +11,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -20,70 +19,78 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import lombok.AllArgsConstructor;
-
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @Configuration
 @AllArgsConstructor
 public class WebSecurityConfig {
+  private final UserDetailsService userDetailsService;
+  private final JWTAuthorizationFilter jwtAuthorizationFilter;
 
-    private final UserDetailsService userDetailsService;
-    private final JWTAuthorizationFilter jwtAuthorizationFilter;
+  @Bean
+  SecurityFilterChain filterChain(
+    HttpSecurity http,
+    AuthenticationManager authenticationManager
+  )
+    throws Exception {
+    JWTAuthenticationFilter jwtAuthenticationFilter = new JWTAuthenticationFilter();
+    jwtAuthenticationFilter.setAuthenticationManager(authenticationManager);
+    jwtAuthenticationFilter.setFilterProcessesUrl("/*");
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
-        JWTAuthenticationFilter jwtAuthenticationFilter = new JWTAuthenticationFilter();
-        jwtAuthenticationFilter.setAuthenticationManager(authenticationManager);
-        jwtAuthenticationFilter.setFilterProcessesUrl("/*");
+    return http
+      .csrf()
+      .disable()
+      .cors()
+      .configurationSource(corsConfigurationSource())
+      .and()
+      .authorizeHttpRequests()
+      .requestMatchers("/**")
+      .permitAll()
+      .anyRequest()
+      .authenticated()
+      .and()
+      .httpBasic()
+      .and()
+      .sessionManagement()
+      .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      .and()
+      .addFilter(jwtAuthenticationFilter)
+      .addFilterBefore(
+        jwtAuthorizationFilter,
+        UsernamePasswordAuthenticationFilter.class
+      )
+      .build();
+  }
 
-        return http
-                .csrf().disable().cors().configurationSource(corsConfigurationSource())
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers("/**").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .addFilter(jwtAuthenticationFilter)
-                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+  @Bean
+  AuthenticationManager authManager(HttpSecurity http) throws Exception {
+    return http
+      .getSharedObject(AuthenticationManagerBuilder.class)
+      .userDetailsService(userDetailsService)
+      .passwordEncoder(passwordEncoder())
+      .and()
+      .build();
+  }
 
-    }
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("*"));
+    configuration.setAllowedMethods(
+      Arrays.asList("GET", "POST", "DELETE", "PUT", "OPTIONS")
+    );
+    configuration.setAllowedHeaders(Arrays.asList("/*"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/*", configuration);
+    return source;
+  }
 
-    @Bean
-    AuthenticationManager authManager(HttpSecurity http) throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder())
-                .and()
-                .build();
-
-    }
-
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("/*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/*", configuration);
-        return source;
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    // public static void main(String[] args) {
-    // System.out.println("pass: " + new
-    // BCryptPasswordEncoder().encode("yoprogramo"));
-    // }
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+  // public static void main(String[] args) {
+  // System.out.println("pass: " + new
+  // BCryptPasswordEncoder().encode("yoprogramo"));
+  // }
 
 }
